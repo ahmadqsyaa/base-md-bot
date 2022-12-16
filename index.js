81cddeb3dedba1ac28b674af28e22c47542d99a9
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2022-12-17 06:26:28 +0700

    handle error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {writeExifImg}= require("./lib/exif.js");
 const { Boom } = require("@hapi/boom");
 const P = require("pino");
 const fs = require('fs');
-
+try{
 const startSock = () => {
     const {state, saveState} = useSingleFileAuthState("session.json"); // NAMA SESION
     const sock = makeWASocket({
@@ -23,12 +23,15 @@ const startSock = () => {
     });
 
       sock.ev.on("connection.update", ({ connection, lastDisconnect})=> { 
-        if (connection === "close") 
+        if (connection == "open") {
+        console.log(connecti);
+//sock.sendMessage('6283173888147@s.whatsapp.net', {text: 'telah Connect!'})
+}
+        
+        if (connection == "close") 
       {
         const error = new Boom(lastDisconnect.error);
         const alasanError = error?.output?.statusCode;
-        
-
         if (alasanError === DisconnectReason.loggedOut) {
           sock.logout();
       } else {
@@ -51,26 +54,34 @@ const startSock = () => {
           if (!m.messages) return;
           if (msg.key && msg.key.remoteJid == "status@broadcast") return
         console.log(msg)
-        const from = msg.key.remoteJid 
-        const isGroup = msg.key.remoteJid.endsWith('@g.us') //KALAU DIGRUP BAKAL DICANCEL
-        const quoted = m.quoted ? m.quoted : m //KAYA UNTUK NGEBALAS PESAN GITU
-        const reply = (teks) => {sock.sendMessage(from, { text: teks }, { quoted: msg })} //UNTUK NGEBALAS TEXT BIAR SIMPLE
+        const from = msg.key.remoteJid
+        const isGroup = msg.key.remoteJid.endsWith('@g.us')
+        const groupMembers = isGroup ? groupMetadata.participants : ''
+        const groupMetadata = isGroup ? await sock.groupMetadata(from) : ''
+        const quoted = m.quoted ? m.quoted : m
+        const reply = (teks) => {sock.sendMessage(from, { text: teks }, { quoted: msg })}
 //≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈//       
         //STICKER 
+        try{
         let caption;
         if(msg.message.imageMessage){
         caption = msg.message.imageMessage.caption
         
         if (caption === 'sticker') {
+        sock.sendPresenceUpdate('composing', msg.message)
         if (isGroup) return reply('pc aja ngab, ntar brisik!')
         reply('proses!')//KALO DI GRUP BAKAL GA RESPON HAPUS AJA KLO GA PENTING//
           let buffer = await downloadMediaMessage(msg, "buffer", {});
           buffer = await writeExifImg(buffer, {packname:"[ ™ ]", author:"[ ©Rick™ ]"});
           sock.sendMessage(from, {sticker:{url: buffer}}, {quoted: msg});
         }
-      }
+      } 
+      } catch (e) {
+console.log(e)
+}
 //≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈//
-        //CHAT RESPON 
+        //CHAT RESPON
+try{
         let pesan;
         if (msg.message.extendedTextMessage)
         pesan = msg.message.extendedTextMessage.text
@@ -82,35 +93,43 @@ const startSock = () => {
         reply('Iyaa? silahkan tinggalkan pesan, dan tunggulah!')
          mo = fs.readFileSync('./2.webp')
         sock.sendMessage(from,{sticker: mo}, {quoted: msg})
-        sock.readMessages([msg.key])//READ MESSAGE
+        sock.readMessages([msg.key])
         }
         
         if(pesan == 'hallo'){
         if(isGroup) return //KALO DI GRUP BAKAL GA RESPON HAPUS AJA KLO GA PENTING//
         sock.sendMessage(from, {text:'hello juga'}, {quoted: msg})
-        sock.readMessages([msg.key])//READ MESSAGE
+        sock.readMessages([msg.key])
         }
         
         if(pesan == 'hai'){
         if(isGroup) return 
-        socksendMessage(from, { text : 'hallo 😌'})
-        sock.readMessages([msg.key])//READ MESSAGE
+        socksendMessage(from, { text : 'hallo 😌'}, {quoted: msg})
+        sock.readMessages([msg.key])
         }
         
         if(pesan == 'ping'){
         if(isGroup) return
-        socksendMessage(from, { text : 'pong'})
-        sock.readMessages([msg.key])//READ MESSAGE
+        socksendMessage(from, { text : 'pong'},{quoted: msg})
+        sock.readMessages([msg.key])
         }
         
-        if(pesan == 'p'){
+        if(pesan == ('p') || pesan == ('P') || pesan == ('Pp') || pesan == ('pe') || pesan == ('ppp')){
         if(isGroup) return
         sock.sendMessage(from, {text: 'Yaa, ada yang bisa saya bantu?'}, {quoted: msg})
-        sock.readMessages([msg.key])//READ MESSAGE
+        sock.readMessages([msg.key])
         }
         
+        
+//KLO ADA ERROR GA BAKAL MATI       
+} catch (e) {
+console.log(e)
+}
 //≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈//
    }); //PENUTUP MESSAGE
 } 
 
-startSock();
+startSock()
+} catch (e) {
+console.log(e)
+}
